7a63dd8c8ae7a43d316f36e2164bca9107cd2a18
YonghoAhn <hrh1110@naver.com>
2018-04-12 13:45:30 +0900

    Feature : AlarmService Done.
    수정 필요하나, 기본적인 알람 서비스 완료. AlarmManager를 사용함

--- app/src/main/java/club/biblelocker/biblelocker/Activities/TestActivity.kt
+++ app/src/main/java/club/biblelocker/biblelocker/Activities/TestActivity.kt
@@ -1,10 +1,15 @@
 package club.biblelocker.biblelocker.Activities
 
 import android.app.AlarmManager
+import android.app.AlarmManager.RTC_WAKEUP
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
 import club.biblelocker.biblelocker.BroadcastReceiver.AlarmReceiver
 import club.biblelocker.biblelocker.MainActivity
@@ -12,19 +17,48 @@ import club.biblelocker.biblelocker.R
 import junit.framework.Test
 import kotlinx.android.synthetic.main.activity_test.*
 import java.util.*
+import java.util.Calendar.HOUR_OF_DAY
+import java.util.Calendar.MINUTE
 
 class TestActivity : AppCompatActivity() {
-
-    var context : Context = applicationContext
-    fun getInstance() : TestActivity { return this }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
-        var alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val calendar = Calendar.getInstance()
         val intent = Intent(applicationContext, AlarmReceiver::class.java)
+
+
+
+        val alarmIntent = Intent(this, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this,0, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        alarm_on.setOnClickListener(View.OnClickListener {
+
+            if (Build.VERSION.SDK_INT < 23)
+            {
+                calendar.set(HOUR_OF_DAY,timePicker.currentHour)
+                calendar.set(MINUTE,timePicker.currentMinute)
+            }
+            else {
+                calendar.set(HOUR_OF_DAY, timePicker.hour)
+                calendar.set(MINUTE, timePicker.minute)
+            }
+
+            Log.d("MisakaMOE",calendar.time.toString())
+
+            alarmManager.set(
+                    RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    pendingIntent
+            )
+
+
+            Log.d("MisakaMOE", "Alarm Set")
+
+        })
 
     }
 }
--- app/src/main/java/club/biblelocker/biblelocker/Services/AlarmService.kt
+++ app/src/main/java/club/biblelocker/biblelocker/Services/AlarmService.kt
@@ -1,27 +1,19 @@
 package club.biblelocker.biblelocker.Services
 
-import android.app.IntentService
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.Service
 import android.content.Intent
-import android.content.Context
-import android.support.v4.app.NotificationCompat
-import club.biblelocker.biblelocker.Activities.TestActivity
-import club.biblelocker.biblelocker.R
+import android.os.IBinder
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 
-class AlarmService : IntentService("AlarmService") {
-
-    override fun onHandleIntent(intent: Intent?) {
-        sendNotification("Alarm")
+class AlarmService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
     }
 
-    private fun sendNotification(string : String)
-    {
-        val alarmManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val pendingIntent = PendingIntent.getActivity(this, 0, Intent(this, TestActivity::class.java),0) as PendingIntent
-        val alarmNotificationBuilder = NotificationCompat.Builder(this,"MisakaMOE").setContentTitle("Alarm").setSmallIcon(R.mipmap.ic_launcher)
-                .setStyle(NotificationCompat.BigTextStyle().bigText(""))
-                .setContentText(string).setContentIntent(pendingIntent)
-        alarmManager.notify(1,alarmNotificationBuilder.build())
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(this,"Alarm",LENGTH_SHORT).show()
+
+        return START_NOT_STICKY
     }
 }
